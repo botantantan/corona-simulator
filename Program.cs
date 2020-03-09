c784bfdcf0bbbc8a4e64d3faeebb5168825b60d8
Fatkhan Masruri <52845259+fatkhanm@users.noreply.github.com>
2020-03-09 22:29:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Tubes2Stima
+{
+    class City
+    {
+        public int populasi;
+        public string nama;
+        public int kenaInfeksi;
+
+        public City()
+        {
+            populasi = 0;
+            kenaInfeksi = 0;
+            nama = "noname";
+            
+        }
+        public City(int populasi, int kenaInfeksi, string nama)
+        {
+
+        }
+        public void set_populasi(int populasi)
+        {
+
+        }
+        public int get_populasi()
+        {
+            return populasi;
+        }
+        public void set_kenaInfeksi(int kenaInfeksi)
+        {
+
+        }
+        public int get_kenaInfeksi()
+        {
+            return kenaInfeksi;
+        }
+        public void set_nama(string nama)
+        {
+
+        }
+        public string get_nama()
+        {
+            return nama;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            City kota = new City(1000,10,"pekutan");
+            Console.WriteLine(kota.get_populasi());
+
+        }
+    }
+}
---
+++ graph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tubes2Stima
+{
+    class Graph
+    {
+        public Boolean check;
+        Dictionary<City,List<City>> Node;
+        Dictionary<KeyValuePair<City, City>, float> probability;
+        Graph Graph = new Graph();
+
+        
+        public Graph()
+        {
+            check = false;
+            Node = new Dictionary<City, List<City>>;
+            probability = new Dictionary<KeyValuePair<City,>>
+        }
+        public void add_Node()
+        {
+
+        }
+
+    }
+}
