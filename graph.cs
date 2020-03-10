38725d3752d2c34e0ed821131c5133d15ca1469e
Fatkhan Masruri <52845259+fatkhanm@users.noreply.github.com>
2020-03-10 21:29:27 +0700

    Update graph.cs

--- graph.cs
+++ graph.cs
@@ -1,25 +1,105 @@
-﻿using System;
+using Microsoft.Msagl.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Tubes2Stima
 {
-    class Graph
+    class GraphCity
     {
-        public Boolean check;
-        Dictionary<City,List<City>> Node;
+        int hariPertama;
+        Dictionary<City, List<City>> Vertex;
         Dictionary<KeyValuePair<City, City>, float> probability;
-        Graph Graph = new Graph();
 
+
+        public GraphCity()
+        {
+            hariPertama = 0;
+            Vertex = new Dictionary<City, List<City>>();
+            probability = new Dictionary<KeyValuePair<City, City>, float>();
+        }
+        public void add_vertex(City kota)
+        {
+            Vertex.Add(kota, new List<City>());
+        }
+        // Menghubungkan vertek asal dengan Tujuan
+        public void add_Edge(City Asal, City Tujuan, float prob)
+        {
+            if (!Vertex.ContainsKey(Asal))
+            {
+                add_vertex(Asal);
+            }
+            if (!Vertex.ContainsKey(Tujuan))
+            {
+                add_vertex(Tujuan);
+            }
+            Vertex[Asal].Add(Tujuan);
+            probability.Add(new KeyValuePair<City, City>(Asal, Tujuan), prob);
+        }
+        public float get_prob(City Asal, City Tujuan)
+        {
+            if (probability.ContainsKey(new KeyValuePair<City, City>(Asal, Tujuan)))
+            {
+                return probability[new KeyValuePair<City, City>(Asal, Tujuan)];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public Boolean has_vertex(City kota)
+        {
+            return Vertex.ContainsKey(kota);
+        }
+        public Boolean has_edge(City Asal, City Tujuan)
+        {
+            return probability.ContainsKey(new KeyValuePair<City, City>(Asal, Tujuan));
+        }
         
-        public Graph()
+        public void  inputFromFile()
         {
-            check = false;
-            Node = new Dictionary<City, List<City>>;
-            probability = new Dictionary<KeyValuePair<City,>>
-        }
-        public void add_Node()
-        {
+            // Input Node
+            string[] lineNode = System.IO.File.ReadAllLines(@"..//..//..//text.txt");
+            bool first = true;
+            
+            foreach(string kata in lineNode)
+            {
+                string[] inputNode = kata.Split(" ");
+                if (first)//line pertama
+                {
+                    foreach(string hruf in inputNode)
+                    {
+                        Console.WriteLine(hruf);
+                        first = false;
 
+                    }
+
+                }
+                else //line lainya
+                {
+                    foreach (string hruf in inputNode)
+                    {
+                    }
+                }
+            }
+          
+            
+          
+           
+
+
+
+
+
+
+            //Input Edge
         }
+
+
+
+
 
     }
 }
