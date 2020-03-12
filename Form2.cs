4a6647afd005166bb6084371e862a23ece428efe
Fatkhan Masruri <52845259+fatkhanm@users.noreply.github.com>
2020-03-12 21:54:56 +0700

    gui form2

---
+++ Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tubes2Stima
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string text)
+        {
+            InitializeComponent();
+           
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
