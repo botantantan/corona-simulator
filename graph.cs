92996e05f635aa81696b13704c9d669cb7cbc888
Fatkhan Masruri <52845259+fatkhanm@users.noreply.github.com>
2020-03-12 21:41:50 +0700

    kontol fix

--- graph.cs
+++ graph.cs
@@ -1,25 +1,34 @@
 
+using Microsoft.Msagl.Drawing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Tubes2Stima
 {
     class GraphCity
     {
-        int hariPertama;
-        Dictionary<City, List<City>> Vertex;
-        Dictionary<KeyValuePair<City, City>, float> probability;
-        Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+        public Dictionary<City, List<City>> Vertex;
+        public Dictionary<KeyValuePair<City, City>, float> probability;
+        public Microsoft.Msagl.Drawing.Graph guiGraph;
+        public Dictionary<string, City> inputanNode;
+        public int jumlahKota;
+        public string kotaAwal;
+        public int jumlahEdge;
+        public Dictionary<int,KeyValuePair<string, string>> jalurInfek;
 
 
         public GraphCity()
         {
-            hariPertama = 0;
             Vertex = new Dictionary<City, List<City>>();
             probability = new Dictionary<KeyValuePair<City, City>, float>();
+            guiGraph = new Microsoft.Msagl.Drawing.Graph();
+            inputanNode = new Dictionary<string, City>();
+            jumlahKota = 0;
+            kotaAwal = "noname";
+            jumlahEdge = 0;
+            jalurInfek = new Dictionary<int,KeyValuePair<string, string>>();
         }
         public void add_vertex(City kota)
         {
@@ -36,14 +45,15 @@ namespace Tubes2Stima
             {
                 add_vertex(Tujuan);
             }
+            guiGraph.AddEdge(Asal.get_nama(), Tujuan.get_nama());
             Vertex[Asal].Add(Tujuan);
             probability.Add(new KeyValuePair<City, City>(Asal, Tujuan), prob);
         }
         public float get_prob(City Asal, City Tujuan)
         {
 
-                return probability[new KeyValuePair<City, City>(Asal, Tujuan)];
-            
+            return probability[new KeyValuePair<City, City>(Asal, Tujuan)];
+
         }
         public Boolean has_vertex(City kota)
         {
@@ -53,44 +63,72 @@ namespace Tubes2Stima
         {
             return probability.ContainsKey(new KeyValuePair<City, City>(Asal, Tujuan));
         }
-        
-        public void  inputFromFile()
+        public void update_Vertex_terinfeksi(City kota)
         {
-            // Input Node
-            string[] lineNode = System.IO.File.ReadAllLines(@"..//..//..//text.txt");
-            bool first = true;
-            
-            foreach(string kata in lineNode)
+            guiGraph.FindNode(kota.get_nama()).Attr.FillColor = Color.Red;
+        }
+        public Edge get_edge(City Asal, City Tujuan)
+        {
+            return guiGraph.Edges.Where(edge => edge.Source == Asal.get_nama() && edge.Target == Tujuan.get_nama()).FirstOrDefault();
+        }
+        public void update_Edge_Terinfeksi(City Asal, City Tujuan)
+        {
+            // guiGraph.AddEdge(Asal.get_nama(), Tujuan.get_nama()).Attr.Color = Color.DarkRed;
+            Edge e = get_edge(Asal, Tujuan);
+            if(e != null)
             {
-                string[] inputNode = kata.Split(" ");
-                if (first)//line pertama
-                {
-                    foreach(string hruf in inputNode)
-                    {
-                        Console.WriteLine(hruf);
-                        first = false;
-
-                    }
-
-                }
-                else //line lainya
-                {
-                    foreach (string hruf in inputNode)
-                    {
-                    }
-                }
+                e.Attr.Color = Color.DarkRed;
             }
-          
-            
-          
-           
+
+            Console.WriteLine(guiGraph.EdgeCount);
+        }
+
+        public void inputFromFile()
+        {
+
+            // Input Node
+            string[] lineNode = System.IO.File.ReadAllLines(@"C:\\Users\\PS42\\Desktop\\Kuliah Semester 4\\STIMA\\Tubes2Stima\\text.txt"); //input Node
+            bool first = true;
 
 
+            foreach (string kata in lineNode)
+            {
+                string[] inputNode = kata.Split(' ');
+                if (first)
+                {
+                    this.jumlahKota = int.Parse(inputNode[0]);
+                    this.kotaAwal = inputNode[1];
+                    first = false;
 
+                }
+                else
+                {
 
+                    this.inputanNode[inputNode[0]] = new City(int.Parse(inputNode[1]), inputNode[0]);
+                    this.guiGraph.AddNode(new Node(inputNode[0]));
 
+                }
+
+            }
 
             //Input Edge
+            string[] lineEdge = System.IO.File.ReadAllLines(@"C:\\Users\\PS42\\Desktop\\Kuliah Semester 4\\STIMA\\Tubes2Stima\\sisi.txt");
+            bool firstEdge = true;
+            foreach (string katae in lineEdge)
+            {
+                string[] inputEdge = katae.Split(' ');
+                if (firstEdge)
+                {
+                    this.jumlahEdge = int.Parse(inputEdge[0]);
+                    firstEdge = false;
+                }
+                else
+                {
+                    add_Edge(this.inputanNode[inputEdge[0]], this.inputanNode[inputEdge[1]], float.Parse(inputEdge[2]));
+                }
+            }
+
+
         }
         //make function penyebaran
         public float f_penyebaran(City Asal, City Tujuan, int day)
@@ -104,11 +142,50 @@ namespace Tubes2Stima
             hari = 20f / (float)(Asal.get_populasi() * get_prob(Asal, Tujuan));
             int nextDay;
             nextDay = (int)(Math.Ceiling(hari));
-            if(nextDay == hari)
+            if (nextDay == hari)
             {
                 ++nextDay;
             }
             return nextDay;
+
+        }
+        public void algortimaBFS(int cekHari)
+        {
+            foreach (City kota in this.Vertex.Keys)
+            {
+                kota.reset();
+            }
+            this.inputanNode[this.kotaAwal].kenaInfeksi = 0;
+            var spanNode = new Queue<City>();
+            spanNode.Enqueue(this.inputanNode[this.kotaAwal]);
+            int count = 0;
+            while (spanNode.Count > 0)
+            {
+                City Check = spanNode.Dequeue();
+                this.update_Vertex_terinfeksi(Check);
+                foreach (City next in this.Vertex[Check])
+                {
+                    
+                    if (this.f_penyebaran(Check, next, cekHari) > 1)
+                    {
+                        update_Edge_Terinfeksi(Check, next);
+                        if (!jalurInfek.ContainsValue(new KeyValuePair<string, string>(Check.get_nama(), next.get_nama())))
+                        {
+                            jalurInfek.Add(count, new KeyValuePair<string, string>(Check.get_nama(), next.get_nama()));
+                            count += 1;
+                        }
+                        
+                        int nextDay = this.first_penyebaran(Check, next) + Check.kenaInfeksi;
+                        if (next.kenaInfeksi <= nextDay)
+                        {
+                            next.kenaInfeksi = nextDay;
+                            spanNode.Enqueue(next);
+                        }
+
+                    }
+                }
+            }
+
 
         }
 
